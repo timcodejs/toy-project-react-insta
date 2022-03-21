fa0f5813aa9a1807ab253735e860f7294fa42313
TimLee <xodnjs479@gmail.com>
2022-03-21 11:10:45 +0900

    ReactJS Toy Project 인스타그램 만들기 - Header 제작

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './header';
+import Container from './container';
+import Userinfo from './userinfo';
+
+const Main = () => {
+    return(
+        <>
+            <Header />
+            <Container />
+            <Userinfo />
+        </>
+    )
+}
+
+export default Main;
---
+++ src/components/userinfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Userinfo = () => {
+    return(
+        <>
+            "Userinfo"
+        </>
+    )
+}
+
+export default Userinfo;
