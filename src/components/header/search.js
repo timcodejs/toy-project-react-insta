6473eb12e9eabb0b071b059b70ce282ad4c175f7
TimLee <xodnjs479@gmail.com>
2022-03-22 11:00:57 +0900

    ReactJS Toy Project 인스타그램 만들기 - 수정

--- src/components/header/search.js
+++ src/components/header/search.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const Search = () => {
     return(
         <MarginDiv>
-            <input type="text" placeholder="검색" />
             <div><svg aria-label="검색" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M19 10.5A8.5 8.5 0 1110.5 2a8.5 8.5 0 018.5 8.5z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="16.511" x2="22" y1="16.511" y2="22"></line></svg></div>
+            <input type="text" placeholder="검색" />
         </MarginDiv>
     )
 }
@@ -25,7 +25,6 @@ const MarginDiv = styled.div`
     }
     div {
         position: absolute;
-        top: 23px; 
-        transform: translateX( 100% );
+        transform: translate( 100%, 45% );
     }
 `;
